4e06d1535f8fdd234f0a7941793285a2816c7a1b
Michał Żygowski <michal.zygowski@3mdeb.com>
2023-11-29 14:23:20 +0100

    Update scripts/flash_ch341.sh

--- scripts/flash_ch341.sh
+++ scripts/flash_ch341.sh
@@ -5,7 +5,7 @@ echo 0 > /sys/class/gpio/gpio199/value
 sleep 5                               
                           
 echo "start flash command"                                
-flashrom -p ch341a_spi --layout ptx01.layout -i bios -w $1
+flashrom -p ch341a_spi --layout flash.layout -i bios -w $1
                                                           
 #In place of $1 provide path to the binary
                                           
