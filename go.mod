234280ef127a05a47be5e314fc15d942cdd8d724
Eduard Kaverinskyi <eduard.kaverinskyi@3mdeb.com>
2025-01-07 15:15:03 +0100

    pkg/gpioControl/gpioControlChardev.go: implement GpioChardev methods
    Signed-off-by: Eduard Kaverinskyi <eduard.kaverinskyi@3mdeb.com>

--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.22.6
 
 toolchain go1.23.4
 
-require github.com/gorilla/mux v1.8.1
+require (
+	github.com/gorilla/mux v1.8.1
+	periph.io/x/conn/v3 v3.7.1
+)
 
 require (
 	github.com/google/go-cmp v0.6.0 // indirect
@@ -25,7 +28,6 @@ require (
 	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
 	golang.org/x/xerrors v0.0.0-20190717185122-a985d3407aa7 // indirect
 	periph.io/x/cmd v0.0.0-20241111231757-5368830fb10a // indirect
-	periph.io/x/conn/v3 v3.7.1 // indirect
 	periph.io/x/d2xx v0.1.1 // indirect
 	periph.io/x/devices/v3 v3.7.2 // indirect
 	periph.io/x/host/v3 v3.8.2 // indirect
--- pkg/gpioControl/gpioControlChardev.go
+++ pkg/gpioControl/gpioControlChardev.go
@@ -1,57 +1,143 @@
 package gpioControl
 
 import (
-	"fmt"
-	"os"
-
 	"3mdeb/RteCtrl/pkg/config"
+	"fmt"
+	"periph.io/x/conn/v3/driver/driverreg"
+	"periph.io/x/conn/v3/gpio"
+	"periph.io/x/conn/v3/gpio/gpioreg"
 )
 
 type GpioChardev struct {
-	sysGpioPath string
-	gpios       map[int]pin
+	sysGpioPath  string
+	gpios        map[int]pin
+	pinDirection map[int]string
 }
 
-func checkGpioChardev(sysGpio uint) bool {
-	target := fmt.Sprintf("%s/gpio%d", ctrl.sysGpioPath, sysGpio)
-	if _, err := os.Stat(target); err == nil {
-		return true
-	}
-
-	return false
+func GetGpioName(gpioNum uint) string {
+	return "GPIO" + fmt.Sprintf("%d", gpioNum)
 }
 
 func NewGpioChardev(gpioPath string, cfg []config.PinConfig) (*GpioChardev, error) {
-	// To be implemented
+	if _, err := driverreg.Init(); err != nil {
+		return nil, err
+	}
+
 	if gpioPath != "" {
 		ctrl.sysGpioPath = gpioPath
 	}
 	var err error
 
-	ctrl.gpios = make(map[int]pin)
+	ctrlChardev := GpioChardev{
+		sysGpioPath: gpioPath,
+		gpios:       make(map[int]pin),
+	}
+
+	ctrlChardev.gpios = make(map[int]pin)
+	ctrlChardev.pinDirection = make(map[int]string)
 
 	for _, val := range cfg {
 		newPin := pin{sysNum: val.SysGpio, description: val.Description}
 		ctrl.gpios[val.ID] = newPin
 
-		if !checkGpio(ctrl.gpios[val.ID].sysNum) {
-			err = exportGpio(ctrl.gpios[val.ID].sysNum)
-			if err != nil {
-				return nil, err
-			}
+		// Check if pin can be found on the system
+		pinName := GetGpioName(val.SysGpio)
+		pin := gpioreg.ByName(pinName)
+		if pin != nil {
+			return nil, err // Return error if we can't find the GPIO pin
 		}
 
-		err = ctrl.SetDirection(val.ID, val.Direction)
+		err = ctrlChardev.SetDirection(val.ID, val.Direction)
 		if err != nil {
 			return nil, err
 		}
 
-		err = ctrl.SetState(val.ID, val.InitValue)
+		err = ctrlChardev.SetState(val.ID, val.InitValue)
 		if err != nil {
 			return nil, err
 		}
 
 	}
 
-	return &ctrl, nil
+	return &ctrlChardev, nil
+}
+
+func (ctrl *GpioChardev) GetPinNumByID(id int) uint {
+	return ctrl.gpios[id].sysNum
+}
+
+func (ctrl *GpioChardev) GetPinNameByID(id int) string {
+	return GetGpioName(ctrl.gpios[id].sysNum)
+}
+
+func (ctrl *GpioChardev) SetDirection(id int, direction string) error {
+	pinID := ctrl.GetPinNumByID(id)
+	pin := gpioreg.ByName(ctrl.GetPinNameByID(id))
+
+	switch direction {
+	case "out", "o":
+		pin.Out(gpio.Low)
+	case "in", "i":
+		fallthrough
+	default:
+		pin.In(gpio.PullNoChange, gpio.NoEdge)
+	}
+	ctrl.pinDirection[int(pinID)] = direction
+
+	return nil
+}
+
+func (ctrl *GpioChardev) GetDirection(id int) (string, error) {
+	pinID := ctrl.GetPinNumByID(id)
+	val, ok := ctrl.pinDirection[int(pinID)]
+	if ok {
+		return val, nil
+	}
+
+	return "", fmt.Errorf("Cannot get the last saved state for pin with id %d", id)
+}
+
+func (ctrl *GpioChardev) SetState(id int, state uint) error {
+	dir, err := ctrl.GetDirection(id)
+	if err != nil {
+		return err
+	}
+	if dir == "in" {
+		return nil
+	}
+
+	pin := gpioreg.ByName(GetGpioName(ctrl.GetPinNumByID(id)))
+
+	switch state {
+	case 0:
+		pin.Out(gpio.Low)
+	case 1:
+		pin.Out(gpio.High)
+	default:
+		err = fmt.Errorf("Unsupported state: %d; Supported states are: [0, 1]")
+		return err
+	}
+
+	return nil
+}
+
+func (ctrl *GpioChardev) GetState(id int) (uint, error) {
+	pin := gpioreg.ByName(GetGpioName(ctrl.GetPinNumByID(id)))
+	state := pin.Read()
+
+	if state == gpio.High {
+		return 1, nil
+	} else if state == gpio.Low {
+		return 0, nil
+	} else {
+		return 0, fmt.Errorf("Unknown state of the pin: %v", state)
+	}
+}
+
+func (ctrl *GpioChardev) GetNumberOfGpios() int {
+	return len(ctrl.gpios)
+}
+
+func (ctrl *GpioChardev) GetDescription(id int) string {
+	return ctrl.gpios[id].description
 }
